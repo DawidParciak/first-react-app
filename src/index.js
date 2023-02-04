77b992dfe2a5f4434d773d42643a64fd4fa2d63e
Dawid <daw.parc@gmail.com>
2023-02-04 20:49:42 +0100

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store.js'
 import App from './App';
-
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+const container = document.querySelector('#root');
+const root = createRoot(container);
+
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
