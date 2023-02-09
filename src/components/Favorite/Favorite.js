4ffea1b498c2bacef104fbb4c6b99282c9eb284d
Dawid <daw.parc@gmail.com>
2023-02-09 20:58:18 +0100

    Change view favorite cards

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -3,33 +3,38 @@ import { getCardFavorite } from '../../redux/store';
 import styles from './Favorite.module.scss';
 import Card from '../Card/Card';
 import PageTitle from '../PageTitle/PageTitle';
+import Button from '../Button/Button';
 
 const Favorite = () => {
 
   const favoriteCards = useSelector(getCardFavorite);
-
-  let emptyFavoriteCards = '';
-
-  if (favoriteCards.length === 0) {
-    emptyFavoriteCards = 'no cards...'
-  }
+  const emptyFavoriteCards = favoriteCards.length === 0;
 
   return (
     <div>
       <PageTitle>Favorite</PageTitle>
-      <article className={styles.column}>
-        <h2 className={styles.title}>{emptyFavoriteCards}</h2>
-        <ul className={styles.cards}>
-          {favoriteCards.map(favoriteCard => 
-            <Card 
-              key={favoriteCard.id} 
-              title={favoriteCard.title} 
-              isFavorite={favoriteCard.isFavorite}
-              cardId={favoriteCard.id}
-            />
-          )}
-        </ul>
-      </article>
+      {emptyFavoriteCards &&
+        <div className={styles.empty}>
+          <h2 className={styles.title}>no cards...</h2>
+          <a href='/'>
+            <Button className={styles.button} onClick={'/'} >Back to home page</Button>
+          </a>
+        </div>
+      }
+      {!emptyFavoriteCards &&
+        <article className={styles.column}>
+          <ul className={styles.cards}>
+            {favoriteCards.map(favoriteCard => 
+              <Card 
+                key={favoriteCard.id} 
+                title={favoriteCard.title} 
+                isFavorite={favoriteCard.isFavorite}
+                cardId={favoriteCard.id}
+              />
+            )}
+          </ul>
+        </article>
+      }
     </div>
   );
 }
